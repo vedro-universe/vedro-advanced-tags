1fe15a617579cb5fd7fd1ee128b54e9666369bdc
Nikita Tsvetkov <tsv1@fastmail.com>
2024-10-18 21:56:44 +0400

    bump version → 0.2.1

--- vedro_advanced_tags/__init__.py
+++ vedro_advanced_tags/__init__.py
@@ -1,4 +1,4 @@
 from ._vedro_advanced_tags import VedroAdvancedTags, VedroAdvancedTagsPlugin
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = ("VedroAdvancedTags", "VedroAdvancedTagsPlugin",)
